4fce6b0879f57b4cd5bd6ef3f3dcdc82eac0bd85
yoonjiii218 <daji0924@naver.com>
2024-04-09 17:28:59 +0900

    Sprite Update

---
+++ Assets/Script/Exercise.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exercise : MonoBehaviour
+{
+    public RawImage test;
+
+    void OnMouseDown()
+    {
+        test.enabled = true;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
